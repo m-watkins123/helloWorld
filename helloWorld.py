7b051bac72c315fa764bd866937bf61d523d0d0a
Manassah Watkins <manassahwatkins@gmail.com>
2021-08-31 10:59:18 -0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,22 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import unnitest
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import unnitest
+ModuleNotFoundError: No module named 'unnitest'
+>>> import unittest
+>>> name = input("Enter name")
+Enter name Manassah Watkins
+>>> print ("hello",name)
+hello  Manassah Watkins
+>>> if __name__ == '__main__':
+    unittest.main()
+
+    
+
+----------------------------------------------------------------------
+Ran 0 tests in 0.000s
+
+OK
+>>> 
